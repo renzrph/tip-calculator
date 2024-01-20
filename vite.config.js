0359d2490d7ada07c2c89031e134f582abffca23
renzrph <renzestorrph@gmail.com>
2024-01-21 00:11:50 +0800

    Icons path fixed 6

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/tip-calculator/",
+  build: {
+    assetsInlineLimit: 0,
+    chunkSizeWarningLimit: 0,
+  },
 });
